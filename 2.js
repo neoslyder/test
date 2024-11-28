9253c51d5f6cfa52bb695f10bd3744d53de27297
neoslyder <56316348+neoslyder@users.noreply.github.com>
2024-11-28 15:48:55 +0100

    Add files via upload

--- 2.js
+++ 2.js
@@ -1,18 +1,8 @@
 (() => {
-  // Fonction pour rechercher et remplacer le texte
-  const replaceTitleText = () => {
-    const headings = document.getElementsByClassName('MuiTypography-root MuiTypography-body2 ean3opc0 css-v0rbmj');;
-    headings.forEach((heading) => {
-     heading.textContent = "Test";
-      }
-    });
-  };
-
-  // Attendre que la page soit entièrement chargée
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", replaceTitleText);
-  } else {
-    replaceTitleText();
-  }
+  var listToModify = document.querySelector("ul");
+var listItemsToShuffle = listToModify.querySelectorAll("li");
+for (var i = 0; i < listItemsToShuffle.length - 1; i++) {
+    listToModify.appendChild(listItemsToShuffle[i]);
+}
 })();
 
